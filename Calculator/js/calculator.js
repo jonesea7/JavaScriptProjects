52f0445a4100b7de9018f390e78061214aedb652
eddywithay <edmundalyn@gmail.com>
2020-08-01 12:51:42 -0400

    Add correct target property and useOperator() to equal-sign-operator class.

--- Calculator/js/calculator.js
+++ Calculator/js/calculator.js
@@ -6,6 +6,8 @@ const Calculator = {
     operator: null,
 };
 
+
+
 //create function that will update display when calc-keys are pushed
 function inputDigit(digitPressed) {
     const {calcScreenValue, readyOperand2} = Calculator;
@@ -15,7 +17,7 @@ function inputDigit(digitPressed) {
     } else {
         Calculator.calcScreenValue = calcScreenValue === "0" ? digitPressed : calcScreenValue + digitPressed;
     }
-};
+}
 
 //what shall we do about decimal points
 function inputDecimal(decimalPoint) {
@@ -23,17 +25,7 @@ function inputDecimal(decimalPoint) {
     if (Calculator.calcScreenValue.includes(decimalPoint)) {
         Calculator.calcScreenValue += decimalPoint; //will add to and include the decimal point to operand
     }
-};
-
-//performCalculation() it was after useOperator() in tutorial
-
-const performCalculation = {
-    '/': (operand1, operand2) => operand1 / operand2,
-    '*': (operand1, operand2) => operand1 * operand2,
-    '+': (operand1, operand2) => operand1 + operand2,
-    '-': (operand1, operand2) => operand1 - operand2,
-    '=': (operand1, operand2) => operand2
-};
+}
 
 //what to do with the operators
 function useOperator(nextOperator) {
@@ -57,41 +49,65 @@ function useOperator(nextOperator) {
     Calculator.operator = nextOperator;
 }
 
+//performCalculation() it was after useOperator() in tutorial
+
+const performCalculation = {
+    '/': (operand1, operand2) => operand1 / operand2,
+    '*': (operand1, operand2) => operand1 * operand2,
+    '+': (operand1, operand2) => operand1 + operand2,
+    '-': (operand1, operand2) => operand1 - operand2,
+    '=': (operand1, operand2) => operand2
+}
+
 //reset the calculator
 function calculatorReset() {
     Calculator.calcScreenValue = '0';
     Calculator.operand1 = null;
     Calculator.readyOperand2 = false;
     Calculator.operator = null;
-};
+}
 
-function updateCalcScreen() {
-    const calcButtons = document.querySelector('.calculator-buttons');
-    calcButtons.addEventListener('click', (event) => {
-        const {elementClicked} = event;
-        if (!elementClicked.matches('button')) {
+function updateCalcScreen () {
+    const display = document.querySelector('.calc-screen');
+    display.value = Calculator.calcScreenValue;
+}
+
+
+
+updateCalcScreen();
+
+const calcButtons = document.querySelector('.calculator-buttons');
+calcButtons.addEventListener('click', (event) => {
+        const {target} = event;
+        if (!target.matches('button')) {
             return;
         }
 
-        if (elementClicked.classList.contains('operator')) {
-            useOperator(elementClicked.value);
+        if (target.classList.contains('operator')) {
+            useOperator(target.value);
             updateCalcScreen();
             return;
         }
 
-        if (elementClicked.classList.contains('decimal-function')) {
-            inputDecimal(elementClicked.value);
+        if (target.classList.contains('decimal-function')) {
+            inputDecimal(target.value);
             updateCalcScreen();
             return;
         }
 
-        if (elementClicked.classList.contains('all-clear-function')) {
+        if (target.classList.contains('all-clear-function')) {
             calculatorReset();
             updateCalcScreen();
             return;
         }
 
-        inputDigit(elementClicked.value);
+        if (target.classList.contains('equal-sign-operator')) { //had to add this because it was a different class in my html
+            useOperator(target.value);
+            updateCalcScreen();
+            return;
+        }
+
+
+        inputDigit(target.value);
         updateCalcScreen();
     })
-};
