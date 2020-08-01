532f7e1fff9c554eef93891f22e7a22d81295b19
eddywithay <edmundalyn@gmail.com>
2020-08-01 10:35:43 -0400

    Add Calulator Project
    Complete html/css
    
    and broken javascript
    
    needs operand2 and event and or elementClicked defined

---
+++ Calculator/js/myCalculator.js
@@ -0,0 +1,17 @@
+//universal var set, which may be an object maybe, can I do it without an object
+
+//function beginCalc() "onkeydown only on the numbered buttons call beginCalc()"
+    //take first operand which must include decimal and end at operator because you need to punch in multiple numbers
+    //take operator and put in var container
+    //take second operand put in var container
+    //if operand1/operator/operand2 !null && '=' is pushed and perform function calculate()
+
+/*function calculate() NOTE: should be built before function beginCalculation
+    return the result of operand1 used with the operator against operand2
+    */
+
+    
+//function allClear()
+    //user display back to zero
+    //operand1 and 2 back to null
+    //operator null
